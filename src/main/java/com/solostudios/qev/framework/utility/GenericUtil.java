659dadda1edc69a0954eac3a512638564c28ad18
solonovamax <solonovamax@12oclockpoint.com>
2019-11-22 17:37:38 -0500

    Added a generic utility class where I can store final variables, etc.

---
+++ src/main/java/com/solostudios/qev/framework/utility/GenericUtil.java
@@ -0,0 +1,13 @@
+package com.solostudios.qev.framework.utility;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class GenericUtil {
+	public static final List<Character> alphabet = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+																 'x', 'y', 'z', ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+	public static final List<String>    morse    = Arrays.asList(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+																 "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "|", ".----", "..---", "...--", "....-",
+																 ".....", "-....", "--...", "---..", "----.", "-----");
+}
