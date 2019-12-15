157748315afbce609b27184e0422b912f66c5aa7
solonovamax <solonovamax@12oclockpoint.com>
2019-12-14 21:11:20 -0500

    Added getPropertyAsList(), load property and a constructor to Properties.java
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/java/com/solostudios/qev/framework/config/Properties.java
+++ src/main/java/com/solostudios/qev/framework/config/Properties.java
@@ -1,12 +1,33 @@
 package com.solostudios.qev.framework.config;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 
 public class Properties {
-	protected final java.util.Properties properties = new java.util.Properties();
+	protected final java.util.Properties properties;
 	
-	protected void loadProperty(ClassLoader classLoader, String propertyFileName) throws IOException {
-		properties.load(classLoader.getResourceAsStream(propertyFileName));
+	public Properties(String propertyDefault) throws IOException, NullPointerException {
+		java.util.Properties defaultProperties = new java.util.Properties();
+		defaultProperties.load(this.getClass().getClassLoader().getResourceAsStream(propertyDefault));
+		
+		properties = new java.util.Properties(defaultProperties);
+	}
+	
+	public Properties() {
+		properties = new java.util.Properties();
+	}
+	
+	protected void loadProperty(ClassLoader classLoader, String propertyFileName) throws IOException, NullPointerException {
+		properties.load(Objects.requireNonNull(classLoader.getResourceAsStream(propertyFileName)));
+	}
+	
+	List<String> getPropertyAsList(String property) {
+		return ((property != null) || (property.length() > 0))
+			   ? Arrays.asList(property.split("\\s*,\\s*"))
+			   : Collections.emptyList();
 	}
 }
